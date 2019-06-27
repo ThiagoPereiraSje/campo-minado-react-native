04ab193803a0647ac6e9ea2acce685f5b90af58f
Thiago Pereira <thiagopereiramail@gmail.com>
2019-06-27 18:29:25 -0300

    Criando o componente mine.

--- mines/src/components/Field.js
+++ mines/src/components/Field.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import params from '../params';
 
+import Mine from './Mine';
+
 export default props => {
-    const { mined, opened, nearMines } = props;
+    const { mined, opened, nearMines, exploded } = props;
 
     const styleField = [styles.field];
     if (opened) styleField.push(styles.opened);
+    if (exploded) styleField.push(styles.exploded);
     if (styleField.length === 1) styleField.push(styles.regular);
 
     let color = null;
@@ -35,6 +38,8 @@ export default props => {
                 <Text style={[ styles.label, { color } ]}> { nearMines } </Text> : 
                 false
             }
+
+            { mined && opened ? <Mine /> : false }
         </View>
     );
 }
@@ -64,5 +69,10 @@ const styles = StyleSheet.create({
     label: {
         fontWeight: 'bold',
         fontSize: params.fontSize,
+    },
+
+    exploded: {
+        backgroundColor: 'red',
+        borderColor: 'red',
     }
 });
