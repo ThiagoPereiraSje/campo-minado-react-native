9ccff5e916df3dbb6d75a803679ff91d99afdbbb
Thiago Pereira <thiagopereiramail@gmail.com>
2019-06-27 17:08:31 -0300

    Criando o componente Field.

--- mines/App.js
+++ mines/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import params from './src/params';
 
+import Field from './src/components/Field';
+
 export default class App extends Component {
   render() {
     return (
@@ -12,6 +14,8 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}
         </Text>
+
+        <Field />
       </View>
     );
   }
